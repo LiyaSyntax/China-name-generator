27c0fd62b516577e11bc8c261536c5a6cb5b77b5
LiyaSyntax <liyaahuman@gmail.com>
2026-04-24 20:35:28 +0700

    Delete lib directory

--- lib/storage.ts
+++
@@ -1,7 +0,0 @@
-export function saveGame(s:any){
-  localStorage.setItem("save",JSON.stringify(s));
-}
-export function loadGame(){
-  const d=localStorage.getItem("save");
-  return d?JSON.parse(d):null;
-}
